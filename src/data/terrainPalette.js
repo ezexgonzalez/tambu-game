fa5256686ca9a3510586660e9dec29a3f80141c3
Ezequiel Gonzalez <88798247+ezexgonzalez@users.noreply.github.com>
2026-09-04 06:14:18 -0300

    chore: document terrain palette

---
+++ src/data/terrainPalette.js
@@ -0,0 +1,8 @@
+// Paleta base compartida por el primer tileset del patio.
+export const TERRAIN_PALETTE = {
+  grass: ['#2c5434', '#315b39', '#274b2f', '#426941'],
+  deck: ['#7a5b43', '#70503b', '#8f6b4e'],
+  path: ['#a4866c', '#b5967a', '#7e6554'],
+  water: ['#228297', '#2a95a8', '#1b6f87', '#5bc4d5'],
+  stone: ['#e0dacd', '#ccc7bb', '#979895'],
+};
