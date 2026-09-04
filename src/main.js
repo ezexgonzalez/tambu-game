ba956643fc9862c925a4ce39cf66fc707d657476
Ezequiel Gonzalez <88798247+ezexgonzalez@users.noreply.github.com>
2026-09-04 06:57:27 -0300

    feat: enable animated Tambu scene

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 import './styles.css';
-import { PatioSceneV011 } from './scenes/PatioSceneV011.js';
+import { PatioSceneV012 } from './scenes/PatioSceneV012.js';
 
 const config = {
   type: Phaser.AUTO,
@@ -22,7 +22,7 @@ const config = {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-  scene: [PatioSceneV011],
+  scene: [PatioSceneV012],
 };
 
 new Phaser.Game(config);
